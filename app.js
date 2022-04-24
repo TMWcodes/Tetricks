ec7a452924c75c99ce38d431325c1719f6677c09
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-04-24 22:26:11 +0100

    tetromino shapes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// var - function + global scoped
+// let - block scoped
+// const - block scoped constant
+
 document.addEventListener('DOMContentLoaded', () => {
     //query selector to look through html for element with class(.) name of div
     const grid = document.querySelector('.grid')
@@ -10,8 +14,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10
 
     console.log(squares)
+
+    //tetrominoes 2d Array
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1,width+2, width*2+2],
+        [1, width+1,width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+    
 })
 
-// var - function + global scoped
-// let - block scoped
-// const - block scoped constant
+//INDEX explained:
+// width = 10
+// [1, width+1, width*2+1, 2]
+
+// after factoring in width:
+// =[01, 11, 21, 02]
+
+// taking those numbers as x and y values:
+// =[(0, 1), (1, 1), (2, 1), (0, 2)
+// ]
+
+// the x and y values indicate which box to colour.
+
+// hope this helps.
+// [0,0]  [0,1]  [0,2]
+// [1,0]  [1,1]  [1,2]
+// [2,0]  [2,1]  [2,2]
+
+
+
+
