d43155ff2c243b75828649b90d6144e54082ccd0
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-04-26 21:27:18 +0100

    random tetrominoes

--- app.js
+++ app.js
@@ -54,7 +54,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
-  let current = theTetrominoes[0][0]
+  let currentRotation = 0
+  //randomly select tetrominoes
+
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+//   console.log(random)
+  let current = theTetrominoes[random][currentRotation]
   
 //   console.log(current)
 
